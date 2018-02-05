c96b48bb584f5f25ab532e0c8e30f90819198ebe
John Vascsinec <pensteeler@gmail.com>
2018-02-05 09:36:38 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const list =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', { class: 'brown' }, 'Chocolate'),
+        React.createElement('li', { class: 'white' }, 'Vanilla'),
+        React.createElement('li', { class: 'yellow' }, 'Banana')
+      ]
+    )
+  );
+ 
+ReactDOM.render(
+  list,
+  document.getElementById('global')
+);
